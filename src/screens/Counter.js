0b1fba2532b57e387e6d8f428debd19668a07a58
Burak Dansık <bburakdnsk@gmail.com>
2025-06-07 05:51:08 +0300

    Counter page button added

--- src/screens/Counter.js
+++ src/screens/Counter.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -6,8 +6,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack= createNativeStackNavigator();
 export default function Counter({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+    <View>
+      <Button title="Arttır" onPress={()=> {}}/>
+      <Button title="Azalt" onPress={()=> {}}/>
+        <Text>Sayı : 0</Text>
     </View>
   );
 }
@@ -18,5 +20,13 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24
+  },Button:{
+    width: "80%",
+    height: 45,
+    backgroundColor: 'purple',
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
   }
 });
