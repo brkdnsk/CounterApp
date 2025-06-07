04faf0acdb91a6191ce5ed956e9910c3005a850d
Burak Dansık <bburakdnsk@gmail.com>
2025-06-07 06:36:17 +0300

    Box App

--- src/screens/BoxPage.js
+++ src/screens/BoxPage.js
@@ -1,15 +1,43 @@
-import { Button, StyleSheet, Text, View ,Button} from 'react-native';
-import React,{useState} from 'react';
+import { StyleSheet, Button, View, FlatList } from 'react-native';
+import React, { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack= createNativeStackNavigator();
-export default function BoxPage({navigation}) {
-    const[colors ,setColors]= useState(second)
+const Stack = createNativeStackNavigator();
+
+export default function BoxPage({ navigation }) {
+  const [colors, setColors] = useState([]);
+
+  const randomColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red},${green},${blue})`;
+  };
+
   return (
     <View>
-     <Button title="Kutu Ekle" onPress={()=>{}}/>
+      <Button
+        title="Kutu Ekle"
+        onPress={() => {
+          setColors([...colors, randomColor()]);
+        }}
+      />
+      <FlatList
+        data={colors}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              height: 150,
+              width: 150,
+              backgroundColor: item,
+              marginVertical: 20,
+            }}
+          />
+        )}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
