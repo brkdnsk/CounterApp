b0a4926756f8ab56da3cacf312c44c4ec2c51017
Burak Dansık <bburakdnsk@gmail.com>
2025-06-07 05:40:23 +0300

    Counter Page

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Counter from '../screens/Counter';
+import CounterScreen from '../screens/CounterScreen';
+
+
+
+const Stack= createNativeStackNavigator();
+
+export default function AppNavigator({navigation}) {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={CounterScreen} />
+        <Stack.Screen name="Counter" component={Counter} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View ,TouchableOpacity} from "react-native";  
+import React from 'react';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack= createNativeStackNavigator();
+export default function CounterScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>SAYAÇ</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Counter')}>
+        <Text style={styles.buttonText}>Sayaca git</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: "80%",
+    height: 45,
+    backgroundColor: 'purple',
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+});
