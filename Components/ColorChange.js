63d0772e313623b7aa743547f902870790e94f83
Burak Dansık <bburakdnsk@gmail.com>
2025-06-08 06:18:35 +0300

    Change Colour

--- Components/ColorChange.js
+++ Components/ColorChange.js
@@ -1,12 +1,12 @@
 import { Button, StyleSheet, Text, View  } from 'react-native'
 import React from 'react'
 
-export default function ColorChange({ colors }) {
+export default function ColorChange({ colors ,onIncrease , onDecrease}) {
   return (
     <View>
       <Text>{colors}</Text>
-      <Button title= {`${colors} arttır`} />
-      <Button title={`${colors} arttır`} />
+      <Button title= {`${colors} arttır`} onPress={()=>onIncrease()} />
+      <Button title={`${colors} arttır`} onPress={()=>onDecrease()} />
 
     </View>
   )
--- src/screens/ColorChangeScreen.js
+++ src/screens/ColorChangeScreen.js
@@ -1,13 +1,20 @@
 import { StyleSheet, Text, View ,Button , FlatList } from 'react-native'
-import React from 'react'
+import React ,{useReducer} from 'react'
 import ColorChange from '../../Components/ColorChange'
 
+const reducer=(state,action)=>{
+
+    console.log(action);
+}
+
+
 export default function ColorChangeScreen() {
+    const [state, dispatch] = useReducer(reducer, {red:0,green:0,blue:0})
   return (
         <View>
-            <ColorChange colors="Kırmızı"/>
-            <ColorChange colors="Mavi"/>
-            <ColorChange colors="Yeşil"/> 
+            <ColorChange colors="Kırmızı" onIncrease={()=>dispatch({type:'changeRed',payload:20})} onDecrease={()=>dispatch({type:'changeRed',payload:-20})}  />
+            <ColorChange colors="Mavi" onIncrease={()=>dispatch({type:'changeBlue',payload:20})} onDecrease={()=>dispatch({type:'changeBlue',payload:-20})}  />
+            <ColorChange colors="Yeşil" onIncrease={()=>dispatch({type:'changeGreen',payload:20})} onDecrease={()=>dispatch({type:'changeGreen',payload:-20})}  /> 
         </View>
   )
 }
