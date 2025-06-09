5afd7855853c608c4ae38760620b2f9a02a91a99
Burak Dansık <bburakdnsk@gmail.com>
2025-06-09 05:19:31 +0300

    Text Input

--- src/screens/PasswordScreen.js
+++ src/screens/PasswordScreen.js
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,TextInput } from 'react-native'
 import React from 'react'
 
 export default function PasswordScreen() {
   return (
-    <View>
-      <Text>şifre</Text>
+    <View style={styles.main}>
+      <Text>Şifrenizi Giriniz</Text>
+      <TextInput style={styles.input}/>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    main:{
+        margin:10,
+    },
+    input:{
+        borderColor:'Black',
+        borderWidth:1,
+    }
+})
